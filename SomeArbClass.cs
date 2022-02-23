397f578996f64da317c9eedf4fdfa5d4af647352
root <root@RAGNAROK.localdomain>
2022-02-23 11:40:02 +0200

    added unit tests

---
+++ SomeArbClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet5.Service.Exp
+{
+    public class SomeArbClass
+    {
+        public string ReverseString(string someString)
+        {
+            return new string(someString.Reverse().ToArray());
+        }
+    }
+}
